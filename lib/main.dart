55662b3b466d99207ee89e4a7b24aa9aa308b53b
Demilade <demiladeoladugba@gmail.com>
2020-09-12 11:22:49 +0100

    configure dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:injectable/injectable.dart';
+import 'package:lsi_mobile/core/configs/dependency_injection/injection.dart';
 import 'package:lsi_mobile/ui/lsi_app.dart';
 
 void main() {
+  configureInjection(Environment.dev);
   runApp(LSIApp());
 }
-
-
-
