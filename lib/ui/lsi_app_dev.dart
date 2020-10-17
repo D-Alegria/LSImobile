09dbde3965aa67a523e356681f53cdfa91967916
Demilade <demiladeoladugba@gmail.com>
2020-10-17 06:28:21 +0100

    Upgrade fkutter

--- lib/ui/lsi_app_dev.dart
+++ lib/ui/lsi_app_dev.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lsi_mobile/core/configs/dependency_injection/injection.dart';
 import 'package:provider/provider.dart';
-import 'package:lsi_mobile/core/configs/route/route.gr.dart';
+import 'package:lsi_mobile/core/configs/route/route.gr.dart' as lsi_r;
 import 'package:lsi_mobile/ui/shared/themes.dart';
 import 'package:lsi_mobile/ui/shared/unknown_route_view.dart';
 import 'package:lsi_mobile/ui/views/authentication/view_model/authentication/authentication_bloc.dart';
@@ -29,9 +29,9 @@ class LSIAppDev extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           theme: themes,
           locale: DevicePreview.of(context).locale,
-          builder: ExtendedNavigator.builder<Router>(
+          builder: ExtendedNavigator.builder<lsi_r.Router>(
             builder: DevicePreview.appBuilder,
-            router: Router(),
+            router: lsi_r.Router(),
             onUnknownRoute: (settings) => unknownRouteView(settings.name),
           ),
         ),
--- lib/ui/lsi_app_prod.dart
+++ lib/ui/lsi_app_prod.dart
@@ -2,7 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lsi_mobile/core/configs/dependency_injection/injection.dart';
-import 'package:lsi_mobile/core/configs/route/route.gr.dart';
+import 'package:lsi_mobile/core/configs/route/route.gr.dart' as lsi_r;
 import 'package:lsi_mobile/core/providers/providers.dart';
 import 'package:lsi_mobile/ui/shared/unknown_route_view.dart';
 import 'package:lsi_mobile/ui/views/authentication/view_model/authentication/authentication_bloc.dart';
@@ -25,8 +25,8 @@ class LSIAppProd extends StatelessWidget {
         providers: providers,
         child: MaterialApp(
           debugShowCheckedModeBanner: false,
-          builder: ExtendedNavigator.builder<Router>(
-            router: Router(),
+          builder: ExtendedNavigator.builder<lsi_r.Router>(
+            router: lsi_r.Router(),
             onUnknownRoute: (settings) => unknownRouteView(settings.name),
           ),
         ),
