25d51003b04879cc2c49e31c800b276c39d81747
Demilade <demiladeoladugba@gmail.com>
2020-11-04 22:51:00 +0100

    refactored loan view

--- lib/ui/views/main/loans/loans_view/loans_view.dart
+++ lib/ui/views/main/loans/loans_view/loans_view.dart
@@ -3,8 +3,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lsi_mobile/core/configs/dependency_injection/injection.dart';
 import 'package:lsi_mobile/ui/shared/shared_wigdets.dart';
 import 'package:lsi_mobile/ui/views/main/loans/active_loan/active_loan_view.dart';
+import 'package:lsi_mobile/ui/views/main/loans/loans_view/view_model/loan_view_cubit.dart';
 import 'package:lsi_mobile/ui/views/main/loans/no_loan_view/no_loan_view.dart';
-import 'package:lsi_mobile/ui/views/main/loans/view_model/loan_view/loan_view_cubit.dart';
 
 class LoansView extends StatelessWidget {
   @override
